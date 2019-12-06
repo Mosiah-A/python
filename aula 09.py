07ce8aec38414d906ec236572382aed3f6ebe43d
Mosiah-A <54685972+Mosiah-A@users.noreply.github.com>
2019-12-06 18:16:15 -0300

    Add files via upload

---
+++ aula 09.py
@@ -0,0 +1,16 @@
+'''frase = 'Curso Em Video python'
+print (frase[9:13]) vide 9 ate 13
+print(frase[9:21:2])
+print(frase[:5])
+print(frase[15:])
+print(frase[9::3]) de três em três
+print(len(frase))
+print(frase.count('o'))
+print(frase.count('o',0,13))
+print(frase.find('deo'))
+print('curso' in frase)'''
+
+print('''oisjisisjid
+owood
+dafafa
+a''')
