4586dc614ca773d91498c510b35b70e1ab8c5a63
Mosiah-A <54685972+Mosiah-A@users.noreply.github.com>
2019-12-23 21:15:11 -0300

    Update ex028.py

--- ex028.py
+++ ex028.py
@@ -1,9 +1,21 @@
-import random
-n = int(random.uniform(0,6))
+#Escrea um programa que faça o computador "Pensar" em um numero inteiro
+# entre 0 e 5 e peça para o usuario tentar descobrir qual foi o numero escolhido
+#pelo computador. O programa devera escrever na tela se o usuario perdeu ou venceu.
+ 
+from random import randint
+from time import sleep
+n = randint(0,5)
+ 
+print('--=--' * 3)
 print('Pensando...')
+print('--=--' * 3)
+sleep(2)
+ 
 r = int(input('Tente adivinhar qual numero de 0-5 eu pensei: '))
+ 
 if n == r:
     print('Acertou')
 else:
     print('Errou')
 print('Eu pensei no numero {}'.format(n))
+ 
